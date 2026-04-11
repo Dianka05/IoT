a131f1f6b4c7152de757104e1ae3e47ab54ce7bb
Estefa7 <kolibri13742@gmail.com>
2026-04-11 18:06:39 +0200

    app.jsx file updated fot logs page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,10 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Dashboard from './pages/dashboard';
 import AdminDashboard from './pages/adminDashboard';
 import DeviceDetails from './pages/DeviceDetails';
+import UsersList from './pages/UsersList';
+import Logs from './pages/Logs';
 
 const Maintenance = () => <div className="p-8">Maintenance Page (In Progress)</div>;
-const Logs = () => <div className="p-8">Logs Page (In Progress)</div>;
 const Settings = () => <div className="p-8">Settings Page (In Progress)</div>;
 
 function App() {
@@ -14,11 +15,12 @@ function App() {
         <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
         <Route path="/dashboard" element={<Dashboard />} />
-         <Route path="/device-details" element={<DeviceDetails />} />
+        <Route path="/device-details" element={<DeviceDetails />} />
         <Route path="/maintenance" element={<Maintenance />} />
         <Route path="/logs" element={<Logs />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="*" element={<div className="p-10">404: Not Found</div>} />
+        <Route path="/users" element={<UsersList />} />
       </Routes>
     </Router>
   );
